af6c1db88556dd3499f7bd6dfd0b30a0042bca10
Nguyen Anh Tuan <55679990+K18rs043@users.noreply.github.com>
2020-07-29 16:35:52 +0900

    create common layout

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+const port = 3000;
+
+app.set("view engine", "pug")
+app.set("views", "./views");
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.render("index");
+});
+
+app.listen(port, function(){
+    console.log("Listening to port " + port);
+});
+
+
+
+
